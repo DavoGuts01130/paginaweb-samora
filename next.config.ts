0b15a865fa494da62c630b3ae4242585b2d337e8
David Ramos <davidramos011@gmail.com>
2026-05-14 13:56:06 -0500

    Fix Supabase images in portfolio

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      // Imágenes de Unsplash
       {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
-
-      // Imágenes almacenadas en Supabase Storage
       {
         protocol: "https",
-        hostname: "zmhkvevkrzpdzgegruajh.supabase.co",
+        hostname: "**.supabase.co",
+        pathname: "/storage/v1/object/public/**",
       },
     ],
   },
